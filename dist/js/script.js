993839a2cf15c479d332359e1bf7e8294bfcaf29
Antitiktod <suryaabdul76@gmail.com>
2024-03-10 06:52:25 +0700

    portofolio tailwind css

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+// Nabar fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixednav = header.offsetTop;
+
+    if(window.scrollY > fixednav) {
+        header.classList.add('navbar-fixed')
+    }
+    else {
+        header.classList.remove('navbar-fixed')
+    }
+}
+
+// hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+})
